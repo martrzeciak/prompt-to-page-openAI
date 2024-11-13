c815e2863b20b53f05e6cbee8e64d821bc27b492
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-11-13 21:34:14 +0100

    Add requirements.txt

--- setup.bat
+++ setup.bat
@@ -7,5 +7,9 @@ REM Activate virtual environment
 echo Activating virtual environment...
 call .venv\Scripts\activate.bat
 
+REM Install dependencies
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+
 echo Setup complete!
 pause
